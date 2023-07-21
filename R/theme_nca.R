f656c230002d8eafee4eb8243d4ef6f13fd12b6f
Nate Apathy <nateapathy@Nates-MacBook-Air.local>
2023-07-21 10:14:48 -0400

    updated theme_nca removed theme_minimal and replace

--- R/theme_nca.R
+++ R/theme_nca.R
@@ -2,50 +2,40 @@
 #
 
 theme_nca <- function() {
-  font <- "AppleGothic"   #assign font family up front
-
-  theme_minimal() %+replace%    # replace elements we want to change
 
     theme(
 
       #grid elements
-      panel.grid.major = element_blank(),    #strip major gridlines
-      panel.grid.minor = element_blank(),    #strip minor gridlines
-      axis.ticks = element_blank(),          #strip axis ticks
-
-      #since theme_minimal() already strips axis lines,
-      #we don't need to do that again
+      panel.grid.major = element_line(color="grey90",linetype=2),
+      panel.grid.minor = element_blank(),
+      panel.background = element_rect(color="grey90"),
+      axis.ticks = element_blank(),
+      plot.title.position = "plot",
+      plot.caption.position = "plot",
 
       #text elements
-      plot.title = element_text(  #title
-        family = font,            #set font family
-        size = 14,                #set font size
-        hjust = 0,                #left align
-        vjust = 2),               #raise slightly
+      plot.title = element_text(
+        family="Helvetica",
+        face="bold",
+        size = rel(1.7),
+        margin=margin(b=7,unit="mm")),
 
-      plot.subtitle = element_text(#subtitle
-        family = font,             #font family
-        face="italic",             #italicize
-        size = 12,
-        hjust=0),                #font size
+      plot.subtitle = element_text(
+        face="italic",
+        size = rel(1.1)),
 
-      plot.caption = element_text(#caption
-        family = font,            #font family
-        size = 9,                 #font size
-        face="italic",            #ital
-        hjust = 0),               #left align
+      plot.caption = element_text(
+        size = 9,
+        hjust = 0,
+        face="italic"),
 
-      axis.title = element_text(             #axis titles
-        family = font,            #font family
-        size = 12),               #font size
+      axis.title = element_text(
+        face="italic",
+        size = rel(1.1)),
 
-      axis.text = element_text(              #axis text
-        family = font,            #axis famuly
-        size = 10),                #font size
+      axis.text = element_text(
+        size = 10),
 
-      axis.text.x = element_text(            #margin for axis text
-        margin=margin(5, b = 10)),
-
-      legend.position="bottom",    # legend on bottom
+      legend.position="bottom",
     )
 }
